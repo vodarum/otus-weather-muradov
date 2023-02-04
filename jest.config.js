cf618f2b3bed154b7890ac1e9ad79ccbfd01c47c
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2023-02-04 16:01:05 +0300

    Return Jest settings

--- jest.config.js
+++ jest.config.js
@@ -132,7 +132,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ["./setupJest.js", "jest-localstorage-mock"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
