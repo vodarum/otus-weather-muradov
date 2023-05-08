a499d80d3f567658beaf57cc7f7ec88464eb6354
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2023-05-08 19:18:31 +0300

    Update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: "./src/js/index",
   output: {
     filename: "main.js",
     clean: true,
